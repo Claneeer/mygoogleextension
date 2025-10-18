bc9ea617d3bf85400643c35949c277c2c6b7491a
Claneeer <pedro.ivoc@sempreceub.com>
2025-10-17 21:57:52 -0300

    teste

--- tests/extension.spec.ts
+++ tests/extension.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect, chromium } from '@playwright/test';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-// Aponta para a pasta de build 'dist', não para a pasta fonte!
+// Recria o '__dirname' para funcionar em Módulos ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Aponta para a pasta de build 'dist', agora da forma correta
 const pathToExtension = path.join(__dirname, '..', 'dist');
 
 test.describe('Testes da Extensão Forçador de Tema', () => {
