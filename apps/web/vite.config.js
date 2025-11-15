0505a47b0aa73d9b3c225c4d8de6a44677caff2a
Claneeer <pedro.ivoc@sempreceub.com>
2025-11-15 11:29:50 -0300

    fix: Adiciona base path para GitHub Pages

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mygoogieextension/', // ADICIONE ESTA LINHA!
+  // O nome deve ser EXATAMENTE o nome do seu repositório GitHub
 })
